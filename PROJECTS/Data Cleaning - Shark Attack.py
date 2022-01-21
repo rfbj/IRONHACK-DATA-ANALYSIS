ddd7f3fb8cd1cee22d621892d10842ec3a3a2a96
rfbj <rafaeljordan27@gmail.com>
2022-01-21 16:57:08 -0300

    dir name change

---
+++ PROJECTS/Data Cleaning - Shark Attack.py
@@ -0,0 +1,13 @@
+##################################
+#### IRONHACK - DATA ANALYSIS ####
+####    MANDATORY PROJECT # 1 ####
+####                          ####
+####       DATA CLEANING      ####
+####        SHARK ATTACK      ####
+##################################
+
+# LIB IMPORTS
+import pandas as pd
+import numpy as np
+import tqdm
+import seaborn as sns
