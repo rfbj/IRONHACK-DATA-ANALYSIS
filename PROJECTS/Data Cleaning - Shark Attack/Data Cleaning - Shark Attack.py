636119d18a869707fa37337e0a0c74bcac72ec9d
rfbj <rafaeljordan27@gmail.com>
2022-01-21 22:01:41 -0300

    update project

--- PROJECTS/Data Cleaning - Shark Attack/Data Cleaning - Shark Attack.py
+++ PROJECTS/Data Cleaning - Shark Attack/Data Cleaning - Shark Attack.py
@@ -11,3 +11,20 @@ import pandas as pd
 import numpy as np
 import tqdm
 import seaborn as sns
+import matplotlib as plt
+import re
+import time
+
+
+
+shark_data = pd.read_csv("data/attacks.csv", encoding="latin1")
+
+start = time.time()
+print(shark_data.head(5))
+print(shark_data.shape)
+shark_data = shark_data[shark_data["Country"] == "USA"]
+print(shark_data)
+end = time.time()
+print(f'Elapsed time: {end - start}')
+
+
